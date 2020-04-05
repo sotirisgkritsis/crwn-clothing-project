6b850f9f4dc1f32279648a359d8058aef00f8be4
Sotirisgkr <sotirisgkr@hotmail.com>
2020-04-05 23:13:41 +0300

    project created and added some files for homepage

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import HomePage from './homepage.components';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage></HomePage>
+    </div>
+  );
+}
+
+export default App;
